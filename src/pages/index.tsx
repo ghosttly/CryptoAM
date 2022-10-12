e1c5acdc0a33d68acb9aa3b55931fb67bec1f451
ghosttly <ghosttlyg@gmail.com>
2022-10-12 19:09:07 +0200

    ini commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Cryptoapp</h1>
+    </div>
+  );
 };
 
 export default Home;
